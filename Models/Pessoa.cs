55ea1a53d4a90e5d8bd8bdefa894e0d4f98e4ac9
MarceloMilbradt <milbradthz@gmail.com>
2021-03-19 21:14:20 -0300

    crei em deus pai

---
+++ Models/Pessoa.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace AssessoriaWeb.Models
+{
+    [Table("Pessoa")]
+    public class Pessoa
+    {
+        [Key]
+        public int pes_id { get; set; }
+        public string pes_nome { get; set; }
+        public string pes_cpf { get; set; }
+        public DateTime pes_datanascimento { get; set; }
+        public string pes_telefone { get; set; }
+        public char pes_sexo { get; set; }
+        public string pes_login { get; set; }
+        public string pes_senha { get; set; }
+        public int pes_tipo { get; set; }
+    }
+}
