54e61761deddca68fbe244beb21482a3f7a5b816
cristianworth <cristianweissmantel@gmail.com>
2021-04-14 20:58:20 -0300

    Add entidade Assessor, Atividade, Treinamento e Turma

---
+++ Models/Atividade.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace AssessoriaWeb.Models
+{
+    [Table("Atividade")]
+    public class Atividade
+    {
+        [Key]
+        [Display(Name = "Atividade")]
+        public int ati_id { get; set; }
+        [Display(Name = "Descrição")]
+        public string ati_descricao { get; set; }
+        [Display(Name = "Observação")]
+        public string atl_observacao { get; set; }
+        public ICollection<Treinamento> Treinamentos { get; set; }
+        public Atividade()
+        {
+            Treinamentos = new HashSet<Treinamento>();
+        }
+    }
+}
---
+++ Models/Treinamento.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace AssessoriaWeb.Models
+{
+    [Table("Treinamento")]
+    public class Treinamento
+    {
+        [Key]
+        [Display(Name = "Treinamento")]
+        public int tre_id { get; set; }
+        [Display(Name = "Data")]
+        public DateTime tre_data { get; set; }
+        [Display(Name = "Hora")]
+        public TimeSpan tre_hora { get; set; }
+        [Display(Name = "Valor")]
+        public decimal tre_valor { get; set; }
+
+
+        [Display(Name = "Assessor")]
+        public int ass_id { get; set; }
+        [Display(Name = "Assessor")]
+        public Assessor Assessor { get; set; }
+
+        [Display(Name = "Atleta")]
+        public int atl_id { get; set; }
+        [Display(Name = "Atleta")]
+        public Atleta Atleta { get; set; }
+        public ICollection<Atividade> Atividades { get; set; }
+        public Treinamento()
+        {
+            Atividades = new HashSet<Atividade>();
+        }
+    }
+}
